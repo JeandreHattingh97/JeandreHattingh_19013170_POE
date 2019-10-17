7e3a0f4468b0221739e6563f6f6be7b870d449a2
JeandreHattingh97 <55444232+JeandreHattingh97@users.noreply.github.com>
2019-10-17 10:00:39 +0200

    Plane and Wizards

--- Assets/Scripts/WizardUnit.cs
+++ Assets/Scripts/WizardUnit.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WizardUnit : MonoBehaviour
+public class WizardUnit : Unit
 {
     // Start is called before the first frame update
     void Start()
